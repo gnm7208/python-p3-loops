5326f428b86ca92eaf1d5ee1b0166444da53260c
George <gnm7208@gmail.com>
2025-11-11 11:04:53 +0300

    Implement fizzbuzz function

--- lib/looping.py
+++ lib/looping.py
@@ -11,5 +11,12 @@ def square_integers(int_list):
     return [num ** 2 for num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
